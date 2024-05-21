5773524584964693902c1a2b7dea3a527392242a
Hayk <hayktunyan1994@gmail.com>
2024-05-21 23:03:02 +0400

    change next config  js

--- next.config.js
+++ next.config.js
@@ -7,21 +7,4 @@ module.exports = {
     defaultLocale: "en",
   },
   output: "standalone",
-  module: {
-    rules: [
-      {
-        test: /\.(mp4|avi|mov|wmv|flv|mkv)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'videos/', // Folder to output the video files
-            },
-          },
-        ],
-      },
-      // ... other rules
-    ],
-  },
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import { appWithTranslation  } from 'next-i18next';
-// import nextI18NextConfig from '../next-i18next.config.js';
-import { Translation, Trans  } from 'react-i18next';
+import { Trans  } from 'react-i18next';
 
 import './i18n';
 import "../css/tailwind.css";
